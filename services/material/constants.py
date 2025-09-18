215824acf48d78f5587114deffbd713e9eced5ba
tr1x_em <admin@trix.is-a.dev>
2025-09-18 20:19:03 +0530

    fix wallpaper

--- services/material/constants.py
+++ services/material/constants.py
@@ -6,6 +6,6 @@ from ignis import utils
 MATERIAL_CACHE_DIR = f"{ignis.CACHE_DIR}/material"  # type: ignore
 
 TEMPLATES = utils.get_current_dir() + "/templates"
-SAMPLE_WALL = utils.get_current_dir() + "../../assets/example_wallpapers/example-3.png"
+SAMPLE_WALL = utils.get_current_dir() + "/sample_wall.png"
 
 os.makedirs(MATERIAL_CACHE_DIR, exist_ok=True)
